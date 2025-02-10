f8c376ee51a2f6dd76dd995134cbcef85cb5cfe6
RaJharit77 <hei.rajo.2@gmail.com>
2025-02-10 14:57:10 +0300

    feat: update all files

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,27 @@
+class TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if Transaction.over_budget?(@transaction.category, @transaction.amount)
+      flash[:alert] = 'Budget exceeded for this category!'
+      render :new
+    else
+      if @transaction.save
+        redirect_to @transaction, notice: 'Transaction was successfully created.'
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:amount, :date, :description, :category_id)
+  end
+end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :category
+
+  def self.over_budget?(category, amount)
+    total = where(category: category).sum(:amount)
+    total + amount > category.budget
+  end
 end
